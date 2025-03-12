a85395ec810464d589584aeae66cf0dc0949c443
benet1one <benet1one@gmail.com>
2025-03-12 22:10:12 +0100

    Check card ownership for debugging

--- klondike.gd
+++ klondike.gd
@@ -13,6 +13,9 @@ func _ready() -> void:
 	$Foundations.position.y = $Stack.position.y
 	$Foundations.position.x = $Stack.position.x + 3 * spacing.x
 
+func _process(_delta: float) -> void:
+	check_card_ownership()
+
 func generate_and_split_cards() -> void:
 	for s in range(4):
 		for n in range(13):
@@ -31,7 +34,7 @@ func generate_and_split_cards() -> void:
 	$Stack.set_cards(stack_cards)
 
 func _on_card_move(card: Card) -> bool:
-	print("Card " + card.format() + " attempting move")
+	print("Card " + str(card) + " attempting move")
 	if card.global_position.y > $Tableau.position.y - 20:
 		return move_to_tableau(card)
 	else:
@@ -56,7 +59,7 @@ func move_to_foundation(card: Card) -> bool:
 	if card.location == Main.Location.Tableau  and  card != $Tableau.tabs[card.tab].back():
 		print("Failed to move Tableau -> Foundations: Card is not last in tab.")
 		return false
-		
+	
 	if card.number != 1:
 		if $Foundations.tabs[card.shape].is_empty():
 			print("Failed to move to Foundations: No cards of shape.")
@@ -81,3 +84,19 @@ func move_to_foundation(card: Card) -> bool:
 	card.row = card.number - 1
 
 	return true
+
+func check_card_ownership():
+	for card in $Stack.stack:
+		if card.get_parent() != $Stack:
+			print("Card in stack has parent ", card.get_parent())
+	for card in $Stack.discard:
+		if card.get_parent() != $Stack:
+			print("Card in discard has parent ", card.get_parent())
+	for t in $Foundations.tabs:
+		for card in t:
+			if card.get_parent() != $Foundations:
+				print("Card in foundations has parent ", card.get_parent())
+	for t in $Tableau.tabs:
+		for card in t:
+			if card.get_parent() != $Tableau:
+				print("Card in tableau has parent ", card.get_parent())

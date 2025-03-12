8df33d9053235bfdf3c5f9a5456eb2a5af15becc
benet1one <benet1one@gmail.com>
2025-03-12 22:16:52 +0100

    Only last card in foundation can be grabbed

--- foundations.gd
+++ foundations.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
-var tabs: Array[Array] = [[], [], [], []]
 var spacing: Vector2
+var tabs: Array[Array] = [[], [], [], []]
+var last_tab_size: Array[int] = [0, 0, 0, 0]
 
 func _process(_delta: float) -> void:
 	for t in range(4):
 		for card in tabs[t]:
 			card.rest_position = Vector2(spacing.x * t, 0)
 			Main.grabbed_on_top(card)
+		
+		if tabs[t].size() != last_tab_size[t]:
+			last_tab_size[t] = tabs[t].size()
+			for card in tabs[t].slice(0, -1):
+				card.disable_grab()
+			if not tabs[t].is_empty():
+				tabs[t].back().enable_grab()
